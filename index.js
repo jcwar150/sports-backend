72a2c83d5e32bdb413d8395c275a414a1e2e69df
jcwar150 <108411319+jcwar150@users.noreply.github.com>
2025-12-27 21:46:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const axios = require("axios");
 
-const ONESIGNAL_APP_ID = "886fb758-5e13-44df-87d4-3f3590e11491"; // tu App ID
-const ONESIGNAL_API_KEY = "os_v2_app_rbx3owc6cncn7b6uh42zbyiusfkfffiogz2esn4vkct2dpsyyovmt7dzo4bqfxmwvgu5mz67bz6a7aymepxhzcaunfoqdvjl4aaeooy"; // tu nueva API Key v2
-
 async function testNotification() {
   try {
     const res = await axios.post("https://onesignal.com/api/v1/notifications", {
-      app_id: ONESIGNAL_APP_ID,
+      app_id: "886fb758-5e13-44df-87d4-3f3590e11491",
       included_segments: ["All"],
-      contents: { en: "Prueba con API Key v2 ✅" }
+      contents: { en: "Prueba con nueva API Key ✅" }
     }, {
       headers: {
-        "Authorization": `Basic ${ONESIGNAL_API_KEY}`, // aquí va la clave v2
+        "Authorization": `Basic ${process.env.ONESIGNAL_API_KEY}`,
         "Content-Type": "application/json"
       }
     });
