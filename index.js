a256353782bbf96b4917936a115a7f57a1bc1480
jcwar150 <108411319+jcwar150@users.noreply.github.com>
2025-12-27 21:30:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-await axios.post("https://onesignal.com/api/v1/notifications", {
-  app_id: ONESIGNAL_APP_ID,
-  included_segments: ["All"],
-  headings: { en: "⚽ Gol en vivo!" },
-  contents: { en: `${home} vs ${away}: ${score}` }
-}, {
-  headers: {
-    "Authorization": `Basic ${os_v2_app_rbx3owc6cncn7b6uh42zbyiushx6e7xyddzutk4aadjbrpwcax7atzbi2odlv5jyrliu5nbqohwm6lj4bbramgct4bck6klklkxbxny}`, // aquí va la REST API Key
-    "Content-Type": "application/json"
-  }
-});
+require('dotenv').config();
+const axios = require('axios');
+
+async function main() {
+  await axios.post("https://onesignal.com/api/v1/notifications", {
+    app_id: "886fb758-5e13-44df-87d4-3f3590e11491",
+    included_segments: ["All"],
+    contents: { en: "Prueba de notificación" }
+  }, {
+    headers: {
+      "Authorization": `Basic TU_REST_API_KEY`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  console.log("✅ Notificación enviada");
+}
+
+main();
 
 
 
